8d28183374276e0be2ae743c7253d4d1de30384d
maximiskorostenskiy-gif <maximIskorostenskiy@gmail.com>
2025-12-18 20:15:30 +0200

    add Student class

---
+++ PR4/main.cpp
@@ -0,0 +1,28 @@
+#ifndef MEDIC_H
+#define MEDIC_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Medic {
+private:
+    string surname;         
+    string specialization; 
+    int experience;         
+
+public:
+    
+    Medic();
+
+  
+    Medic(string s, string spec, int exp);
+
+  
+    void printInfo() const;
+
+    ~Medic();
+};
+
+#endif
