f83240a5afdb8652940c1bc086b500bf310f4e59
maximiskorostenskiy-gif <maximIskorostenskiy@gmail.com>
2025-12-18 20:22:57 +0200

    do PR4

--- PR4/main.cpp
+++ PR4/main.cpp
@@ -1,28 +1,66 @@
-#ifndef MEDIC_H
-#define MEDIC_H
-
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-class Medic {
+class Product
+{
 private:
-    string surname;         
-    string specialization; 
-    int experience;         
+    string name;
+    string manufacturer;
+    double price;
 
 public:
-    
-    Medic();
+    Product()
+    {
+        this->name = "";
+        this->manufacturer = "";
+        this->price = 0.0;
+    }
 
-  
-    Medic(string s, string spec, int exp);
+    Product(string name, string manufacturer, double price)
+    {
+        this->name = name;
+        this->manufacturer = manufacturer;
+        this->price = price;
+    }
 
-  
-    void printInfo() const;
-
-    ~Medic();
+    void showProduct()
+    {
+        cout << "Name: " << name << ", Manuf: " << manufacturer << ", Price: " << price << endl;
+    }
 };
 
-#endif
+class Medic : public Product
+{
+private:
+    string specialization;
+    int experience;
+
+public:
+    Medic() : Product()
+    {
+        this->specialization = "";
+        this->experience = 0;
+    }
+
+    Medic(string specialization, int experience, string name, string manufacturer, double price) : Product(name, manufacturer, price)
+    {
+        this->specialization = specialization;
+        this->experience = experience;
+    }
+
+    void showMedic()
+    {
+        showProduct();
+        cout << "Specialization: " << specialization << ", Experience: " << experience << endl;
+    }
+};
+
+int main()
+{
+    Medic myMedic("Surgeon", 10, "Dr. House", "Princeton", 5000.0);
+    myMedic.showMedic();
+
+    return 0;
+}
